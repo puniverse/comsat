70c23cba9a15b92807aac4e2e44067afa8fb05a5
pron <ron.pressler@gmail.com>
2013-10-30 12:30:32 +0200

    Added jetty classloader

---
+++ comsat-jetty-loader/src/main/java/co/paralleluniverse/comsat/jetty/QuasarWebAppClassLoader.java
@@ -0,0 +1,51 @@
+/*
+ * COMSAT
+ * Copyright (C) 2013, Parallel Universe Software Co. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 3.0
+ * as published by the Free Software Foundation.
+ */
+package co.paralleluniverse.comsat.jetty;
+
+import co.paralleluniverse.fibers.instrument.QuasarURLClassLoaderHelper;
+import java.io.IOException;
+import java.io.InputStream;
+import org.eclipse.jetty.webapp.WebAppClassLoader;
+
+
+
+/**
+ * See:
+ * http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
+ *
+ * @author pron
+ */
+public class QuasarWebAppClassLoader extends WebAppClassLoader {
+    private final QuasarURLClassLoaderHelper helper;
+
+    public QuasarWebAppClassLoader(Context context) throws IOException {
+        super(context);
+        this.helper = new QuasarURLClassLoaderHelper(this);
+    }
+
+    public QuasarWebAppClassLoader(ClassLoader parent, Context context) throws IOException {
+        super(parent, context);
+        this.helper = new QuasarURLClassLoaderHelper(this);
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        return helper.findClass(name);
+    }
+
+    @Override
+    public InputStream getResourceAsStream(String name) {
+        return helper.getResourceAsStream(name);
+    }
+}
