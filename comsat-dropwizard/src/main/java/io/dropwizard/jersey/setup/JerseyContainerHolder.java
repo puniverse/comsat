19be0f373bf955f2a3f642bf77ee9ee35dfccbdf
eitan <eitan@paralleluniverse.co>
2014-05-07 13:51:55 +0300

    include dropwizaed changes in comsat

---
+++ comsat-dropwizard/src/main/java/io/dropwizard/jersey/setup/JerseyContainerHolder.java
@@ -0,0 +1,20 @@
+package io.dropwizard.jersey.setup;
+
+import com.sun.jersey.spi.container.servlet.ServletContainer;
+import javax.servlet.Servlet;
+
+public class JerseyContainerHolder {
+    private Servlet container;
+
+    public JerseyContainerHolder(ServletContainer container) {
+        this.container = container;
+    }
+
+    public Servlet getContainer() {
+        return container;
+    }
+
+    public void setContainer(Servlet container) {
+        this.container = container;
+    }
+}
