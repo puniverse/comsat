26625b9e2973bed216b0a87cac7d31c15a187edb
eitan <eitan@paralleluniverse.co>
2014-06-12 14:53:36 +0300

    change FiberRestAdaptherBuilder to inherit from retrofit

--- comsat-retrofit/src/main/java/co/paralleluniverse/fibers/retrofit/FiberRestAdaptherBuilder.java
+++ comsat-retrofit/src/main/java/co/paralleluniverse/fibers/retrofit/FiberRestAdaptherBuilder.java
@@ -1,73 +1,46 @@
 package co.paralleluniverse.fibers.retrofit;
 
+import co.paralleluniverse.fibers.httpclient.FiberHttpClient;
 import co.paralleluniverse.fibers.httpclient.FiberHttpClientBuilder;
-import java.util.concurrent.Executor;
 import org.apache.http.client.HttpClient;
-import retrofit.Endpoint;
-import retrofit.ErrorHandler;
-import retrofit.Profiler;
-import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
 import retrofit.client.ApacheClient;
 import retrofit.client.Client;
-import retrofit.converter.Converter;
 
-public class FiberRestAdaptherBuilder {
-    private final RestAdapter.Builder builder;
+public class FiberRestAdaptherBuilder extends RestAdapter.Builder {
+    private HttpClient httpClient;
 
-    public FiberRestAdaptherBuilder() {
-        this(FiberHttpClientBuilder.create().setUserAgent("").build());
-    }
-    
-    public FiberRestAdaptherBuilder(HttpClient client) {
-        this.builder = new RestAdapter.Builder().setClient(new ApacheClient(client));
-    }
-
-    public RestAdapter.Builder setEndpoint(String endpoint) {
-        return builder.setEndpoint(endpoint);
-    }
-
-    public RestAdapter.Builder setEndpoint(Endpoint endpoint) {
-        return builder.setEndpoint(endpoint);
-    }
-
-    public RestAdapter.Builder setClient(Client client) {
-        return builder.setClient(client);
-    }
-
+    @Override
     public RestAdapter.Builder setClient(Client.Provider clientProvider) {
-        return builder.setClient(clientProvider);
+        throw new UnsupportedOperationException("Only Fiber Apache Http clients are allowed here. Use setClient(FiberHttpClient) instead.");
     }
 
-    public RestAdapter.Builder setExecutors(Executor httpExecutor, Executor callbackExecutor) {
-        return builder.setExecutors(httpExecutor, callbackExecutor);
+    @Override
+    public RestAdapter.Builder setClient(Client client) {
+        throw new UnsupportedOperationException("Only Fiber Apache Http clients are allowed here. Use setClient(FiberHttpClient) instead.");
+
     }
 
-    public RestAdapter.Builder setRequestInterceptor(RequestInterceptor requestInterceptor) {
-        return builder.setRequestInterceptor(requestInterceptor);
+    public RestAdapter.Builder setClient(FiberHttpClient client) {
+        this.httpClient = client;
+        return this;
     }
 
-    public RestAdapter.Builder setConverter(Converter converter) {
-        return builder.setConverter(converter);
-    }
-
-    public RestAdapter.Builder setProfiler(Profiler profiler) {
-        return builder.setProfiler(profiler);
-    }
-
-    public RestAdapter.Builder setErrorHandler(ErrorHandler errorHandler) {
-        return builder.setErrorHandler(errorHandler);
-    }
-
-    public RestAdapter.Builder setLog(RestAdapter.Log log) {
-        return builder.setLog(log);
-    }
-
-    public RestAdapter.Builder setLogLevel(RestAdapter.LogLevel logLevel) {
-        return builder.setLogLevel(logLevel);
-    }
-
+    @Override
     public RestAdapter build() {
-        return builder.build();
+        if (httpClient == null)
+            // setUserAgent("") is not default it ApacheHttpClient, and is needed for the github API  
+            this.httpClient = FiberHttpClientBuilder.create().setUserAgent("").build();
+        super.setClient(providerFor(new ApacheClient(httpClient)));
+        return super.build();
+    }
+
+    private static Client.Provider providerFor(final Client client) {
+        return new Client.Provider() {
+            @Override
+            public Client get() {
+                return client;
+            }
+        };
     }
 }
