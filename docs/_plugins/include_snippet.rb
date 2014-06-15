1f0fcea25854ab727afda7e51f394b18102b7b78
eitan <eitan@paralleluniverse.co>
2014-06-15 11:33:49 +0300

    add include_snippet plugin

---
+++ docs/_plugins/include_snippet.rb
@@ -0,0 +1,69 @@
+# Title: Include Snippet Tag for Jekyll
+# Author: Eitan Yarden https://github.com/eitan101
+# Description: Import your code snippets into any blog post.
+# Configuration: You can set default import path in _config.yml (defaults to code_dir: downloads/code)
+#
+# Syntax {% include_snippet snippet_name path/to/file %}
+#
+# Example 1:
+# ~~~ java
+# {% include_code hello world javascripts/test.java %}
+# ~~~
+#
+# This will import test.js starting from the line which contains "snippet hello world" till
+# the line which contains "end of snippet". For code higlight wrap the tag with the appropritate md tag.
+#
+#
+
+require 'pathname'
+
+module Jekyll
+
+  class IncludeCodeTag < Liquid::Tag
+    # include HighlightCode
+    # include TemplateWrapper
+    def initialize(tag_name, markup, tokens)
+      @title = nil
+      if markup.strip =~ /(.*)?(\s+|^)(\/*\S+)/i
+        @title = $1 || nil
+        @file = $3
+      end
+      super
+    end
+
+    def render(context)
+      code_dir = (context.registers[:site].config['code_dir'].sub(/^\//,'') || 'downloads/code')
+      code_path = (Pathname.new(context.registers[:site].source) + code_dir).expand_path
+      file = code_path + @file
+
+      if File.symlink?(code_path)
+        return "Code directory '#{code_path}' cannot be a symlink"
+      end
+
+      unless file.file?
+        return "File #{file} could not be found"
+      end
+
+      Dir.chdir(code_path) do
+        code = file.read
+        source = ""
+        inblock = false;
+        code.lines.each_with_index do |line,index|
+          if line =~ /end of snippet/
+            inblock = false
+          end
+          if inblock
+              source  += "#{line}"
+          end
+          if line =~ /snippet #{@title}/
+            inblock = true
+          end
+        end
+        ouput = source
+      end
+    end
+  end
+
+end
+
+Liquid::Template.register_tag('include_snippet', Jekyll::IncludeCodeTag)
