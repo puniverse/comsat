528df26bd00b5459545facd547544236420fb6b5
eitan <eitan@paralleluniverse.co>
2014-06-17 23:25:24 +0300

    init comsat-test-war subproject

---
+++ comsat-test-war/src/main/java/co/paralleluniverse/examples/test/TestServlet.java
@@ -0,0 +1,23 @@
+package co.paralleluniverse.examples.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * This servlet is used by the RS-client tests
+ */
+@WebServlet(urlPatterns = "/test")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (PrintWriter out = resp.getWriter()) {
+            out.println(new Date() + ": test servlet response");
+        }
+    }
+}
