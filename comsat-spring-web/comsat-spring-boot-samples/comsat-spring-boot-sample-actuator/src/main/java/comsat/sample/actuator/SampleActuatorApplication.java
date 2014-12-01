3ad36385122231ccf78e69b1c44779dcdd4fe18b
circlespainter <fabio@paralleluniverse.co>
2014-12-01 16:05:31 +0100

    Minor test adjustments [ci skip]

--- comsat-spring-web/comsat-spring-boot-samples/comsat-spring-boot-sample-actuator/src/main/java/comsat/sample/actuator/SampleActuatorApplication.java
+++ comsat-spring-web/comsat-spring-boot-samples/comsat-spring-boot-sample-actuator/src/main/java/comsat/sample/actuator/SampleActuatorApplication.java
@@ -19,15 +19,17 @@
  */
 package comsat.sample.actuator;
 
+import co.paralleluniverse.springframework.web.servlet.config.annotation.FiberWebMvcConfigurationSupport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
-//@Import(FiberWebMvcConfigurationSupport.class) // This will enable fiber-blocking
+@Import(FiberWebMvcConfigurationSupport.class) // This will enable fiber-blocking
 @EnableAutoConfiguration
 @EnableConfigurationProperties
 @ComponentScan
