71ac2d62dcef66bc09899343463b2c62de6b305b
pron <ron.pressler@gmail.com>
2014-12-23 23:03:51 +0200

    Add :scm element in project.clj to make Clojars happy

--- comsat-ring-jetty9/project.clj
+++ comsat-ring-jetty9/project.clj
@@ -1,6 +1,7 @@
 (defproject co.paralleluniverse/comsat-ring-jetty9 "0.3.0"
   :description "Comsat integration for the Ring Clojure web framework: Jetty 9 fiber-blocking adapter."
   :url "https://github.com/puniverse/comsat"
+  :scm {:name "git" :url "https://github.com/puniverse/comsat"}
 
   :licenses [{:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
              {:name "GNU Lesser General Public License - v 3" :url "http://www.gnu.org/licenses/lgpl.html"}]
