2b2562083ca27d669542d6a3c8c4e1d50cd018d7
pron <ron.pressler@gmail.com>
2014-12-23 23:03:32 +0200

    -

--- comsat-ring-jetty9/project.clj
+++ comsat-ring-jetty9/project.clj
@@ -2,11 +2,9 @@
   :description "Comsat integration for the Ring Clojure web framework: Jetty 9 fiber-blocking adapter."
   :url "https://github.com/puniverse/comsat"
 
-  ; TODO Check if it works for POM generation to have two under :licenses, Leiningen's sample doesn't list this case
   :licenses [{:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
              {:name "GNU Lesser General Public License - v 3" :url "http://www.gnu.org/licenses/lgpl.html"}]
 
-  ; TODO Check if it works to have it outside of license(s), Leiningen's sample doesn't list this case
   :distribution :repo
 
   :min-lein-version "2.4.3"
