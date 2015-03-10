13d5712bb79bf5fa854c360558f76c98ceb63949
circlespainter <fabio@paralleluniverse.co>
2015-03-10 11:20:12 +0200

    OkHttp Square ©

--- comsat-okhttp/src/test/java/co/paralleluniverse/fibers/okhttp/internal/SingleInetAddressNetwork.java
+++ comsat-okhttp/src/test/java/co/paralleluniverse/fibers/okhttp/internal/SingleInetAddressNetwork.java
@@ -1,17 +1,20 @@
 /*
- * Copyright (C) 2014 Square, Inc.
+ * COMSAT
+ * Copyright (c) 2013-2015, Parallel Universe Software Co. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *   or (per the licensee's choosing)
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * under the terms of the GNU Lesser General Public License version 3.0
+ * as published by the Free Software Foundation.
+ */
+/*
+ * Based on the corresponding class in okhttp-tests.
+ * Copyright 2014 Square, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License").
  */
 package co.paralleluniverse.fibers.okhttp.internal;
 
--- comsat-okhttp/src/test/java/com/squareup/okhttp/DispatcherTest.java
+++ comsat-okhttp/src/test/java/com/squareup/okhttp/DispatcherTest.java
@@ -1,3 +1,21 @@
+/*
+ * COMSAT
+ * Copyright (c) 2013-2015, Parallel Universe Software Co. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 3.0
+ * as published by the Free Software Foundation.
+ */
+/*
+ * Based on the corresponding class in okhttp-tests.
+ * Copyright 2014 Square, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ */
 package com.squareup.okhttp;
 
 import co.paralleluniverse.fibers.okhttp.FiberOkHttpClient;
