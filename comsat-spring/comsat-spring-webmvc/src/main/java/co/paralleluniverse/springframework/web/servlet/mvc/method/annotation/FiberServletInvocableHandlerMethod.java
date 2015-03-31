425547e8bbb1d2b8e1f912617f7bf9c54388c656
circlespainter <fabio@paralleluniverse.co>
2015-03-31 19:27:50 +0300

    [ci skip]

--- comsat-spring/comsat-spring-webmvc/src/main/java/co/paralleluniverse/springframework/web/servlet/mvc/method/annotation/FiberServletInvocableHandlerMethod.java
+++ comsat-spring/comsat-spring-webmvc/src/main/java/co/paralleluniverse/springframework/web/servlet/mvc/method/annotation/FiberServletInvocableHandlerMethod.java
@@ -17,7 +17,6 @@
  * Copyright the original author Rossen Stoyanchev.
  * Released under the ASF 2.0 license.
  */
-
 package co.paralleluniverse.springframework.web.servlet.mvc.method.annotation;
 
 import java.io.IOException;
