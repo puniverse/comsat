438c0fc7f041576855a1134d55fc4962e0bc1b51
circlespainter <fabio@paralleluniverse.co>
2015-04-01 15:33:05 +0300

    [ci skip]

--- comsat-spring/comsat-spring-boot/comsat-spring-boot-sample-actuator-log4j2/src/main/java/comsat/sample/actuator/log4j2/ServiceProperties.java
+++ comsat-spring/comsat-spring-boot/comsat-spring-boot-sample-actuator-log4j2/src/main/java/comsat/sample/actuator/log4j2/ServiceProperties.java
@@ -24,15 +24,13 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "service", ignoreUnknownFields = false)
 @Component
 public class ServiceProperties {
+    private String name = "World";
 
-	private String name = "World";
+    public String getName() {
+            return this.name;
+    }
 
-	public String getName() {
-		return this.name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
+    public void setName(String name) {
+            this.name = name;
+    }
 }
