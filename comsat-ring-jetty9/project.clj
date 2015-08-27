b2907a565504c0c9ee805d2892c87aea7747e171
circlespainter <fabio@paralleluniverse.co>
2015-08-27 22:28:20 +0300

    :arrow_up: Upgrade 'comsat-ring' deps

--- comsat-ring-jetty9/project.clj
+++ comsat-ring-jetty9/project.clj
@@ -1,4 +1,4 @@
-(def quasar-pulsar-version "0.7.2")
+(def quasar-pulsar-version "0.7.3-SNAPSHOT")
 
 (defproject co.paralleluniverse/comsat-ring-jetty9 "0.5.0-SNAPSHOT"
   :description "Comsat integration for the Ring Clojure web framework: Jetty 9 fiber-blocking adapter."
@@ -13,19 +13,21 @@
   :min-lein-version "2.5.0"
 
   :dependencies
-    [[org.clojure/clojure "1.6.0"]
+    [[org.clojure/clojure "1.7.0"]
 
-     [ring/ring-core "1.3.2"]
-     [ring/ring-devel "1.3.2"]
-     [ring/ring-servlet "1.3.2"]
+     [ring/ring-core "1.4.0"]
+     [ring/ring-devel "1.4.0"]
+     [ring/ring-servlet "1.4.0"]
 
-     [org.eclipse.jetty/jetty-server "9.3.0.v20150612"]
+     [org.eclipse.jetty/jetty-server "9.3.2.v20150730"]
 
      [org.slf4j/slf4j-simple "1.7.12"]
 
      [co.paralleluniverse/pulsar ~quasar-pulsar-version]]
 
+  :repositories [["snapshots" "http://oss.sonatype.org/content/repositories/snapshots"]]
+
   :java-agents [[co.paralleluniverse/quasar-core ~quasar-pulsar-version]]
 
   :profiles
-    {:dev {:dependencies [[clj-http "1.1.2"]]}})
+    {:dev {:dependencies [[clj-http "2.0.0"]]}})
