c67a9668481b7c68a528c7b9475f747b5abe6edb
Tim Brooks <tim@uncontended.net>
2015-10-17 15:57:41 -0500

    Add test for callback send

--- comsat-kafka/src/test/java/co/paralleluniverse/fibers/kafka/FiberKafkaProducerTest.java
+++ comsat-kafka/src/test/java/co/paralleluniverse/fibers/kafka/FiberKafkaProducerTest.java
@@ -13,6 +13,7 @@
  */
 package co.paralleluniverse.fibers.kafka;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.MockProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
@@ -21,6 +22,7 @@ import org.junit.Test;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.Assert.assertEquals;
 
@@ -36,7 +38,7 @@ public class FiberKafkaProducerTest {
     }
 
     @Test
-    public void testSendWithoutCallback() throws ExecutionException, InterruptedException {
+    public void testSuccessfulSendWithoutCallback() throws ExecutionException, InterruptedException {
         Future<RecordMetadata> f = fiberProducer.send(new ProducerRecord<>("Topic", "Key".getBytes(), "Value".getBytes()));
         RecordMetadata recordMetadata = f.get();
 
@@ -51,5 +53,46 @@ public class FiberKafkaProducerTest {
         assertEquals("Topic", recordMetadata2.topic());
         assertEquals(1, recordMetadata2.offset());
         assertEquals(0, recordMetadata2.partition());
+    }
+
+    @Test
+    public void testSuccessfulSendWithCallback() throws ExecutionException, InterruptedException {
+        final AtomicReference<RecordMetadata> callbackMetadata = new AtomicReference<>(null);
+        Future<RecordMetadata> f = fiberProducer.send(new ProducerRecord<>("Topic", "Key".getBytes(), "Value"
+                .getBytes()), new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception exception) {
+                callbackMetadata.set(metadata);
+
+            }
+        });
+        RecordMetadata recordMetadata = f.get();
+
+        assertEquals("Topic", recordMetadata.topic());
+        assertEquals(0, recordMetadata.offset());
+        assertEquals(0, recordMetadata.partition());
+
+        assertEquals("Topic", callbackMetadata.get().topic());
+        assertEquals(0, callbackMetadata.get().offset());
+        assertEquals(0, callbackMetadata.get().partition());
+
+        final AtomicReference<RecordMetadata> callbackMetadata2 = new AtomicReference<>(null);
+        Future<RecordMetadata> f2 = fiberProducer.send(new ProducerRecord<>("Topic", "Key".getBytes(),
+                "Value".getBytes()), new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception exception) {
+                callbackMetadata2.set(metadata);
+
+            }
+        });
+        RecordMetadata recordMetadata2 = f2.get();
+
+        assertEquals("Topic", recordMetadata2.topic());
+        assertEquals(1, recordMetadata2.offset());
+        assertEquals(0, recordMetadata2.partition());
+
+        assertEquals("Topic", callbackMetadata2.get().topic());
+        assertEquals(1, callbackMetadata2.get().offset());
+        assertEquals(0, callbackMetadata2.get().partition());
     }
 }
