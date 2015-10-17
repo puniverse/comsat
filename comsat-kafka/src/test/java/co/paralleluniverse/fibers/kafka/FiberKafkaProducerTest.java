ed1538bef12943a70af706743e17a97012eb2958
Tim Brooks <tim@uncontended.net>
2015-10-17 15:17:53 -0500

    Add basic test for producer

--- comsat-kafka/src/test/java/co/paralleluniverse/fibers/kafka/FiberKafkaProducerTest.java
+++ comsat-kafka/src/test/java/co/paralleluniverse/fibers/kafka/FiberKafkaProducerTest.java
@@ -13,11 +13,43 @@
  */
 package co.paralleluniverse.fibers.kafka;
 
+import org.apache.kafka.clients.producer.MockProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static org.junit.Assert.assertEquals;
 
 public class FiberKafkaProducerTest {
 
+    private MockProducer mockProducer;
+    private FiberKafkaProducer<byte[], byte[]> fiberProducer;
+
+    @Before
+    public void setUp() {
+        mockProducer = new MockProducer();
+        fiberProducer = new FiberKafkaProducer<>(mockProducer);
+    }
+
     @Test
-    public void testThing() {
+    public void testSendWithoutCallback() throws ExecutionException, InterruptedException {
+        Future<RecordMetadata> f = fiberProducer.send(new ProducerRecord<>("Topic", "Key".getBytes(), "Value".getBytes()));
+        RecordMetadata recordMetadata = f.get();
+
+        assertEquals("Topic", recordMetadata.topic());
+        assertEquals(0, recordMetadata.offset());
+        assertEquals(0, recordMetadata.partition());
+
+        Future<RecordMetadata> f2 = fiberProducer.send(new ProducerRecord<>("Topic", "Key".getBytes(),
+                "Value".getBytes()));
+        RecordMetadata recordMetadata2 = f2.get();
+
+        assertEquals("Topic", recordMetadata2.topic());
+        assertEquals(1, recordMetadata2.offset());
+        assertEquals(0, recordMetadata2.partition());
     }
 }
