c441997f5b29e08255adf3f07f81ce3da6ce2de2
circlespainter <fabio@paralleluniverse.co>
2015-11-24 19:15:03 +0200

    :arrow_up: Upgrade all deps except OkHttp

--- comsat-spring/comsat-spring-boot/comsat-spring-boot-sample-tomcat-jsp/src/test/java/comsat/sample/jsp/SampleWebJspApplicationTests.java
+++ comsat-spring/comsat-spring-boot/comsat-spring-boot-sample-tomcat-jsp/src/test/java/comsat/sample/jsp/SampleWebJspApplicationTests.java
@@ -49,6 +49,7 @@ public class SampleWebJspApplicationTests {
 
     @Test
     public void testJspWithEl() throws Exception {
+        System.out.println("Sleeping...");
         ResponseEntity<String> entity = new TestRestTemplate().getForEntity("http://localhost:" + this.port, String.class);
         assertEquals(HttpStatus.OK, entity.getStatusCode());
         assertTrue("Wrong body:\n" + entity.getBody(), entity.getBody().contains("/resources/text.txt"));
