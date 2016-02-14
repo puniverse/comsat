69dc5ec20245bc035f4f0c8c619de4831507e12a
circlespainter <fabio@paralleluniverse.co>
2016-02-14 12:09:32 +0200

    -

--- comsat-tomcat-loader/src/main/java/co/paralleluniverse/comsat/tomcat/QuasarWebAppClassLoader.java
+++ comsat-tomcat-loader/src/main/java/co/paralleluniverse/comsat/tomcat/QuasarWebAppClassLoader.java
@@ -1,6 +1,6 @@
 /*
  * COMSAT
- * Copyright (c) 2013-2014, Parallel Universe Software Co. All rights reserved.
+ * Copyright (c) 2013-2016, Parallel Universe Software Co. All rights reserved.
  *
  * This program and the accompanying materials are dual-licensed under
  * either the terms of the Eclipse Public License v1.0 as published by
@@ -28,10 +28,9 @@ import org.apache.catalina.loader.WebappClassLoader;
  *
  * @author pron
  */
-public class QuasarWebAppClassLoader extends WebappClassLoader {
-
+public final class QuasarWebAppClassLoader extends WebappClassLoader {
     @Override
-    protected synchronized boolean filter(String name) {
+    protected final synchronized boolean filter(String name) {
         // Don't re-load the instrumentation logic, including the `SuspendableClassifier` interface,
         // else implementations in the webapp classloader will have trouble loading when running in
         // a standalone servlet container
@@ -44,25 +43,24 @@ public class QuasarWebAppClassLoader extends WebappClassLoader {
 
     private QuasarInstrumentor instrumentor;
 
-    public QuasarWebAppClassLoader() {
-    }
+    public QuasarWebAppClassLoader() {}
 
     public QuasarWebAppClassLoader(ClassLoader parent) {
         super(parent);
     }
 
     private QuasarInstrumentor newInstrumentor() {
-        QuasarInstrumentor inst = new QuasarInstrumentor(this); // must be called *after* construction has completed
+        final QuasarInstrumentor inst = new QuasarInstrumentor(this); // must be called *after* construction has completed
         inst.setLog(new Log() {
             @Override
-            public void log(LogLevel level, String msg, Object... args) {
-                System.out.println("[quasar] " + level + ": " + String.format(msg, args));
+            public final void log(LogLevel level, String msg, Object... args) {
+                System.err.println("[quasar] " + level + ": " + String.format(msg, args));
             }
 
             @Override
-            public void error(String msg, Throwable exc) {
-                System.out.println("[quasar] ERROR: " + msg);
-                exc.printStackTrace(System.out);
+            public final void error(String msg, Throwable exc) {
+                System.err.println("[quasar] ERROR: " + msg);
+                exc.printStackTrace(System.err);
             }
         });
         inst.setVerbose(false);
@@ -72,20 +70,20 @@ public class QuasarWebAppClassLoader extends WebappClassLoader {
 
     private synchronized void initInstrumentor() {
         if (instrumentor == null)
-            this.instrumentor = newInstrumentor();
+            instrumentor = newInstrumentor();
     }
 
     @Override
-    protected ResourceEntry findResourceInternal(String name, String path, boolean manifestRequired) {
+    protected final ResourceEntry findResourceInternal(String name, String path, boolean manifestRequired) {
         initInstrumentor();
-        ResourceEntry entry = super.findResourceInternal(name, path, manifestRequired);
+        final ResourceEntry entry = super.findResourceInternal(name, path, manifestRequired);
         if (entry != null && path.endsWith(".class") && entry.binaryContent != null) {
-            String className = name.substring(0, name.length() - ".class".length());
+            final String className = name.substring(0, name.length() - ".class".length());
             try {
-                byte[] res = instrumentor.instrumentClass(className, entry.binaryContent);
+                final byte[] res = instrumentor.instrumentClass(className, entry.binaryContent);
                 if (res != null)
                     entry.binaryContent = res;
-            } catch (Exception ex) {
+            } catch (final Exception ex) {
                 if (MethodDatabase.isProblematicClass(className))
                     instrumentor.log(LogLevel.INFO, "Skipping problematic class instrumentation %s - %s %s", className, ex, Arrays.toString(ex.getStackTrace()));
                 else
