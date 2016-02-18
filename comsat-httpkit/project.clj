4a2d03c28151af64b421bb28d04315d747ce5abb
circlespainter <fabio@paralleluniverse.co>
2016-02-18 10:59:33 +0200

    Next dev iteration (Clojure parts)

--- comsat-httpkit/project.clj
+++ comsat-httpkit/project.clj
@@ -1,6 +1,6 @@
 (def quasar-pulsar-version "0.7.4")
 
-(defproject co.paralleluniverse/comsat-httpkit "0.6.0"
+(defproject co.paralleluniverse/comsat-httpkit "0.7.0-SNAPSHOT"
   :description "'httpkit' Quasar integration"
 
   :url "https://github.com/puniverse/comsat"
